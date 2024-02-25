1d51a3323fdb39601d7ab549cb86e4e490b07fb2
kduraj <konrad.m.duraj@gmail.com>
2024-02-25 17:22:44 +0100

    Updated gaussian membership

--- deezzy/memberships/gaussian.py
+++ deezzy/memberships/gaussian.py
@@ -5,6 +5,11 @@ class Gaussian(torch.nn.Module):
         super(Gaussian, self).__init__()
         self.univariate=univariate
 
+    def _compute_inverse_cov_matrix(self, variance:torch.Tensor):
+        b,c,g,f = variance.size()
+        covariance_matrix = torch.pow((torch.cat([torch.eye(f) for _ in range(b*c*g)]).view(b,c,g,f,f) * variance.unsqueeze(dim=-1)),2)
+        return torch.inverse(covariance_matrix)
+
     def forward(self, x:torch.Tensor, representation:torch.Tensor):
         if self.univariate:
             batch_size, num_features, granularity, _ = representation.size()
@@ -13,5 +18,14 @@ class Gaussian(torch.nn.Module):
             z = (x.unsqueeze(dim=-1) - mean)/var
             return torch.exp(-0.5 * torch.pow(z,2))
         else:
-            pass
+            batch_size, num_classes,num_gaussians, num_features, num_parameters = representation.size()
+            mean = representation.view(batch_size, num_classes*num_gaussians*num_features, num_parameters)[...,0].view(batch_size, num_classes, num_gaussians,num_features)
+            var = representation.view(batch_size, num_classes*num_gaussians*num_features, num_parameters)[...,1].view(batch_size, num_classes, num_gaussians,num_features)
+
+            inverse_matrix = self._compute_inverse_cov_matrix(var)
+            diff = x.view(batch_size, 1, 1, num_features) - mean
+            mm1 = diff.view(batch_size, num_classes, num_gaussians, 1, num_features) @ inverse_matrix
+            mm2 = mm1 @ diff.view(batch_size, num_classes, num_gaussians, num_features,1)
+            z = -.5 * mm2.view(batch_size, num_classes, num_gaussians)
+            return torch.exp(z)
         
